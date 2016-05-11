74b8612e7817201a68d521f743cb6b9c73f59560
Ricardo Rigodon <Ricardo Rigodon>
2016-05-11 16:31:46 -0400

    Refactoring for TakeHome. Added method combos to take color strings create all possible strings and put into an array list. Then, check every value in the list and check if that resides in the hashmap and if the length is the right length. Now text file won't need lines upon lines of the same string in different orders.

--- app/src/main/java/match/ColorMatcher.java
+++ app/src/main/java/match/ColorMatcher.java
@@ -34,11 +34,17 @@ import ricardo.colormatch.data.ColorItems;
  * Uses hashmap to store values from matches file, deciphers colors by HSV, and processes
  * if the colors match or not and return to user.
  *
+ *
+ * Added for final takeHome : combos method that creates an ArrayList and creates all possible
+ * permutations of the string given stores in list and that is passed into .containsValueOf for the
+ * hashMap instead of the one string. Now matches.txt will not have to have multiple strings of the
+ * same match in order for the value to have the correct value returned.
+ *
  * @author Ricardo Rigodon
  */
 
 
-public class ColorMatcher{
+public class ColorMatcher {
 
 
     //boolean value that will be used to return that will determine if color is a match or not.
@@ -46,8 +52,8 @@ public class ColorMatcher{
     private static boolean match = false;
 
     //red
-    private final static int LRHUE = 180; //low range of hue
-    private final static int HRHUE = 390; // high range of hue
+    private final static int LRHUE = 0; //low range of hue
+    private final static int HRHUE = 80; // high range of hue
     private final static int LRVA = 52; // low range of value
     private final static int HRVA = 100; // high range of value
     private final static int RSAT = 75; // saturation value
@@ -77,7 +83,7 @@ public class ColorMatcher{
     private final static int HGHUE = 180; // high range of hue
     private final static int LGVA = 20; // low range of value
     private final static int HGVA = 100; // high range of value
-    private final static int GSAT = 85; // saturation value
+    private final static int GSAT = 96; // saturation value
 
     //yellow
     private final static int LYHUE = 35; // low range of hue
@@ -105,57 +111,53 @@ public class ColorMatcher{
     private static String yourColors = ""; //string of colors
 
     //Creates a hashmap with integer as key and strings as the value.
-    private static Map<Integer,String> map = new HashMap<Integer,String>();
+    private static Map<Integer, String> map = new HashMap<Integer, String>();
 
 
-
-    /*
+    /**
         Takes into the context where it is called and the list of savedColorItems that the user
         entered.
 
         @param context The context from which the method was called.
         @param list The list of saved color items that the user currently chose
-     */
+     **/
     public static boolean isMatch(Context context, List<ColorItem> list) {
 
         AssetManager assetManager = context.getAssets(); //get assets from context
         BufferedReader reader = null; //create BufferedReader
+        //match to false here in case it never reset
         match = false;
 
-        try{
+        try {
             //create new buffered reader and open input stream in matches.txt
             reader = new BufferedReader(new InputStreamReader(assetManager.open("matches.txt")));
 
             String mLine = ""; //line
 
-            while((mLine = reader.readLine()).length() > 1){
+            while ((mLine = reader.readLine()).length() > 1) {
 
                 String parts[] = mLine.split("="); //split values on =
                 parts[0] = parts[0].trim(); // remove whitespace
                 int m = Integer.parseInt(parts[0]); // parseInt
-                map.put(m , parts[1].trim()); // put in hashmap
+                map.put(m, parts[1].trim()); // put in hashMap
             }
             reader.close(); // close reader
 
 
-
-        }catch(IOException e){
+        } catch (IOException e) {
 
             e.printStackTrace(); //print trace of stack
         }
 
 
-
-
         //Grab the size of the list of color items and store that
         int count = list.size(); //grab size of list
-        ColorItem[] colors = new ColorItem[10]; // create array to hold color item
+        ColorItem[] colors = new ColorItem[count]; // create array to hold color item
         String[] hsvparts = new String[count]; //array to hold split of hsv parts
         Integer[] hue = new Integer[count]; //holds hue values
         Integer[] sat = new Integer[count];//holds saturation value
         Integer[] val = new Integer[count];//holds value from hsv
         String[] myColors = new String[count]; //array that holds
-
 
         for (int i = 0; i < list.size(); i++) {
 
@@ -173,28 +175,28 @@ public class ColorMatcher{
             int va = Integer.parseInt(hsvparts[2]); // value
 
             //Store the HSV values in the corresponding arrays
-            hue[i] = hu;
-            sat[i] = sa;
-            val[i] = va;
+            hue[i] = hu; //H of HSV
+            sat[i] = sa; // S of HSV
+            val[i] = va; // V of HSV
 
             /*
-              The colors will be determined by analyzing the hue, saturation,
-               and value of each color item.
-
+             * The colors will be determined by analyzing the hue, saturation,
+             * and value of each color item.
              */
 
             //red
-            if ((hu >= LRHUE && hu <= HRHUE) && (va >= LRVA && va <= HRVA ) && (sa >= RSAT)) {
 
-                myColors[i] = "r"; //set to string of r
+            if ((hu >= LRHUE && hu <= HRHUE) && (va >= LRVA && va <= HRVA) && (sa >= RSAT)) {
+
+                myColors[i] = "r"; //set to string of r (red)
 
             }
 
             //yellow
 
-            else if ((hu >= LYHUE && hu <= HYHUE) && (va >= LYVA && va <= HYVA) && (sa >= LYSAT  && sa <= HYSAT)) {
+            else if ((hu >= LYHUE && hu <= HYHUE) && (va >= LYVA && va <= HYVA) && (sa >= LYSAT && sa <= HYSAT)) {
 
-                myColors[i] = "y"; //set to string of y
+                myColors[i] = "y"; //set to string of y (yellow)
 
             }
 
@@ -203,14 +205,14 @@ public class ColorMatcher{
 
             else if ((hu >= LGHUE && hu <= HGHUE) && (va >= LGVA && va <= HGVA) && (sa <= GSAT)) {
 
-                myColors[i] = "g"; //set to string of g
+                myColors[i] = "g"; //set to string of g (green)
 
             }
 
 
             //cyan
 
-            else if ((hu >= LCHUE && hu < HCHUE) && (va >= LCVA && va < HCVA) && (sa <= CSAT)) {
+            else if ((hu >= LCHUE && hu <= HCHUE) && (va >= LCVA && va <= HCVA) && (sa <= CSAT)) {
 
                 myColors[i] = "c"; // set to string of c (cyan)
 
@@ -237,9 +239,9 @@ public class ColorMatcher{
 
             //white
 
-           else if ((hu >= LWHUE && hu < HWHUE) && (va >= LWVA && va < HWVA) && (sa <= WSAT)) {
+            else if ((hu >= LWHUE && hu < HWHUE) && (va >= LWVA && va < HWVA) && (sa <= WSAT)) {
 
-                myColors[i] = "w"; // set to string of w
+                myColors[i] = "w"; // set to string of w (white)
 
             }
 
@@ -248,40 +250,56 @@ public class ColorMatcher{
 
             else if ((hu >= LBLHUE && hu < HBLHUE) && (va < BLVA) && (sa <= BLSAT)) {
 
-                myColors[i] = "bl"; //set to string of bl
+                myColors[i] = "bl"; //set to string of bl (black)
 
             }
 
 
             //Assume the color is white having trouble of camera being able to identify it.
-
-           else if (myColors[i] == null) {
-                myColors[i] = "w"; // set to string of w
+            // Avoids issue of null color for now.
+            else if (myColors[i] == null) {
+                myColors[i] = "w"; // set to string of w (white)
             }
 
 
-
-            Log.d("Color", myColors[i]);
-
         }
 
-        for(int i = 0; i < myColors.length; i++){
+
+        for (int i = 0; i < myColors.length; i++) {
 
             yourColors += myColors[i]; //add to yourColors string
 
         }
 
+        //store returned arrayList in stringCombos variable [takeHome]
+        ArrayList<String> stringCombos = combos(yourColors);
 
+        //boolean value that is set true if arraylist contains a value that is in hashmap loaded
+        //by matches.txt [takeHome]
+        boolean check = false;
 
-        //check if map has the color string in the matches file
-        if(map.containsValue(String.valueOf(yourColors))){
+        //check if map has any permutations of color string inside the hashmap of matches.
+        //added to make more efficient than adding more lines in text file. [takeHome]
+        for(int i = 0; i < stringCombos.size(); i++){
 
+            if(map.containsValue(String.valueOf(stringCombos.get(i)))){
+                //check to make sure only strings of length of list are checked against hashMap
+                if(stringCombos.get(i).length() == count)
+                check = true;
+            }
+        }
+
+        //if it is in the hashMap it is a match
+        if(check){
             match = true;
         }
 
 
         //reset color string
         yourColors = "";
+
+        //clear ArrayList
+        stringCombos.clear();
 
 
         //return match value
@@ -290,6 +308,41 @@ public class ColorMatcher{
 
     }
 
+    /**
+       *  Combos is a method that returns an ArrayList<String> of all the possible combinations
+       *  of the characters into a string inside it. More efficient than
+       * originally writing all possible permutations in matches.txt file. Added for Take Home Final.
+       * Perk of ArrayList, more modular as more colors are added don't have to worry about resizing.
+       *  @param  colors   String of colors ex. "wcbl"
+     **/
+
+    public static ArrayList<String> combos(String colors) {
+
+        //ArrayList to hold all possible permutations of string
+        ArrayList<String> combos = new ArrayList<String>();
+        // will store length of arrayList as it is changing.
+        int length;
+
+        for(int i = 0; i < colors.length(); i++){
+
+            //grab size of list as it changes
+            length = combos.size();
+
+            for(int j = 0; j < length; j++){
+                //add list element and appended the list element at index j to it
+                combos.add(colors.charAt(i) + combos.get(j));
+            }
+            //adds charAt index i to the array list
+            combos.add(Character.toString(colors.charAt(i)));
+
+        }
+
+        //return arrayList of all the strings
+        return combos;
+
+
     }
+
+}
 
 
